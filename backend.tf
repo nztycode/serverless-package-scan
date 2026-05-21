05388aea14366af5228785d9343bd10566b5805a
NZTYCODE <NEZ7296@GMAIL.COM>
2026-05-21 22:56:21 +0800

    terraform-checks

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce12-tfstate"
+    bucket = "sctp-ce12-tfstate-bucket"
     key    = "package-vul-scan-nas.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
     region = "ap-southeast-1"
   }
--- provider.tf
+++ provider.tf
@@ -1,13 +1,18 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "ap-southeast-1" # or your preferred region
+  region = "ap-southeast-1"
 }
